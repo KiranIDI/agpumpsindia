6c0595fe18f281d3af965e261ff99c65d41b6e03
Kiran Kumar <kirankumar17897@gmail.com>
2021-02-18 15:38:57 +0530

    I HP Two Stage Self Priming Mono Block Pump Page Updated

---
+++ assets/php/header.php
@@ -0,0 +1,246 @@
+<!doctype html>
+<html class="no-js" lang="en">
+	
+<head>
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<title>Team | Tionscal</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- favicon -->		
+		<link rel="shortcut icon" type="image/x-icon" href="img/logo/favicon.ico">
+
+		<!-- all css here -->
+
+		<!-- bootstrap v3.3.6 css -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<!-- owl.carousel css -->
+		<link rel="stylesheet" href="css/owl.carousel.css">
+		<link rel="stylesheet" href="css/owl.transitions.css">
+        <!-- meanmenu css -->
+        <link rel="stylesheet" href="css/meanmenu.min.css">
+		<!-- font-awesome css -->
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/icon.css">
+		<link rel="stylesheet" href="css/flaticon.css">
+		<!-- magnific css -->
+        <link rel="stylesheet" href="css/magnific.min.css">
+		<!-- venobox css -->
+		<link rel="stylesheet" href="css/venobox.css">
+		<!-- style css -->
+		<link rel="stylesheet" href="css/style.css">
+		<!-- responsive css -->
+		<link rel="stylesheet" href="css/responsive.css">
+
+		<!-- modernizr css -->
+		<script src="js/vendor/modernizr-2.8.3.min.js"></script>
+	</head>
+		<body>
+
+		<!--[if lt IE 8]>
+			<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+		<![endif]-->
+
+
+<div id="preloader"></div>
+<header class="header-one">
+ <!-- Start top bar -->
+ <div class="topbar-area fix hidden-xs">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-10 col-sm-9">
+              <div class="topbar-left">
+                <ul>
+                  <li>
+                    <a href="#"
+                      ><i class="fa fa-map-marker"></i> House-4,2/3
+                      avenew,Dubai</a
+                    >
+                  </li>
+                  <li class="hidden-sm">
+                    <a href="#"
+                      ><i class="fa fa-clock-o"></i> Mon - Fri: 10:00 - 18:00</a
+                    >
+                  </li>
+                  <li>
+                    <a href="#"
+                      ><i class="fa fa-envelope"></i> info@tionscall3.com</a
+                    >
+                  </li>
+                  <li>
+                    <a href="#"><i class="fa fa-phone"></i> +2112-6546654</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-2 col-sm-3">
+              <div class="quote-button">
+                <a
+                  href="contact.html"
+                  class="quote-btn"
+                  title="Quick view"
+                  data-toggle="modal"
+                  data-target="#quoteModal"
+                  >Get a quote</a
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- End top bar -->
+      <!-- header-area start -->
+      <div id="sticker" class="header-area hidden-xs">
+        <div class="container">
+          <div class="row">
+            <!-- logo start -->
+            <div class="col-md-3 col-sm-3 logo-bg">
+              <div class="logo">
+                <!-- Brand -->
+                <a
+                  class="navbar-brand page-scroll sticky-logo"
+                  href="index-2.html"
+                >
+                  <img src="img/logo/logo22.png" alt="" />
+                </a>
+              </div>
+            </div>
+            <!-- logo end -->
+            <div class="col-md-9 col-sm-9">
+              <div class="header-right-link">
+                <!-- search option start -->
+                <form action="#">
+                  <div class="search-option">
+                    <input type="text" placeholder="Search..." />
+                    <button class="button" type="submit">
+                      <i class="fa fa-search"></i>
+                    </button>
+                  </div>
+                  <a class="main-search" href="#"
+                    ><i class="fa fa-search"></i
+                  ></a>
+                </form>
+                <!-- search option end -->
+              </div>
+              <!-- mainmenu start -->
+              <nav class="navbar navbar-default">
+                <div class="collapse navbar-collapse" id="navbar-example">
+                  <div class="main-menu">
+                    <ul class="nav navbar-nav navbar-right">
+                      <li><a class="pagess" href="index.html">Home</a></li>
+                      <li><a class="pagess" href="about.html">About us</a></li>
+                      <li>
+                        <a class="pagess" href="#">Products</a>
+                        <ul class="sub-menu">
+                          <li>
+                            <a href="openwellsubmersible_pumps.html"
+                              >Openwell Submersible Pumps</a
+                            >
+                          </li>
+                          <li>
+                            <a href="singlephaseopenwellsubmersiblepumps.html"
+                              >Single Phase Open Well Submersible Pumps</a
+                            >
+                          </li>
+                          <li>
+                            <a href="v4&v6submersible_pumps.html"
+                              >v4 & v6 Submersible Pumps</a
+                            >
+                          </li>
+                          <li>
+                            <a href="selfpriming_monoblock.html"
+                              >Selfpriming Monoblock</a
+                            >
+                          </li>
+                          <li>
+                            <a
+                              href="1hp_two_stage_self_priming_mono_block_pump.html"
+                              >1hp Two Stage Self Priming Mono Block Pump</a
+                            >
+                          </li>
+                          <li>
+                            <a href="borewell_compressor.html"
+                              >Borewell Compressor</a
+                            >
+                          </li>
+                        </ul>
+                      </li>
+
+                      <li>
+                        <a class="pagess" href="bare_motors.html">Motors</a>
+                      </li>
+                      <li><a href="#">contacts</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+              <!-- mainmenu end -->
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- header-area end -->
+      <!-- mobile-menu-area start -->
+      <div class="mobile-menu-area hidden-lg hidden-md hidden-sm">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="mobile-menu">
+                <div class="logo">
+                  <a href="index-2.html"
+                    ><img src="img/logo/logo1.png" alt=""
+                  /></a>
+                </div>
+                <nav id="dropdown">
+                  <ul>
+                    <li><a class="pagess" href="index.html">Home</a></li>
+                    <li><a class="pagess" href="about.html">About us</a></li>
+                    <li><a class="pagess" href="#">Products</a></li>
+                    <ul class="sub-menu">
+                      <li>
+                        <a href="openwellsubmersible_pumps.html"
+                          >Openwell Submersible Pumps</a
+                        >
+                      </li>
+                      <li>
+                        <a href="singlephaseopenwellsubmersiblepumps.html"
+                          >Single Phase Open Well Submersible Pumps</a
+                        >
+                      </li>
+                      <li>
+                        <a href="v4&v6submersible_pumps.html"
+                          >v4 & v6 Submersible Pumps</a
+                        >
+                      </li>
+                      <li>
+                        <a href="selfpriming_monoblock.html"
+                          >Selfpriming Monoblock</a
+                        >
+                      </li>
+                      <li>
+                        <a
+                          href="1hp_two_stage_self_priming_mono_block_pump.html"
+                          >1hp Two Stage Self Priming Mono Block Pump</a
+                        >
+                      </li>
+                      <li>
+                        <a href="borewell_compressor.html"
+                          >Borewell Compressor</a
+                        >
+                      </li>
+                    </ul>
+                    <li>
+                      <a class="pagess" href="bare_motors.html">Motors</a>
+                    </li>
+                    <li><a href="#">contacts</a></li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- mobile-menu-area end -->
+    </header>
+<!-- header end -->
